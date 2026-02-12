536f261fa19960d6062e154fafdb4df0c42f585f
kifayat <kifayatdildar125@gmail.com>
2026-02-11 23:09:50 -0800

    Added React project

---
+++ react-tailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import ProfileCard from "./ProfileCard";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+    <ProfileCard name="Tayyaba" role="React Developer" />
+<ProfileCard name="Tayyaba" role="React Developer" />
+
+    </div>
+  );
+}
+
+export default App;
+
